c87de0c0d25c70788d66b5230258ea3826f15d2b
Sraidi2001 <sraidimohamedreda0@gmail.com>
2023-06-15 15:31:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A PROGRAM THAT PRIRTS A LINE WITH PUTS FUNCTION
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
