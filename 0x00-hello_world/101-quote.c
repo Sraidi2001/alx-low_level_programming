70d095bcd01c53ed51f3df3a8907b00e66b9446c
Sraidi2001 <sraidimohamedreda0@gmail.com>
2023-06-15 17:04:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - program that prints the size of various types on the computer
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
